5be372e6c2c0f87b737c0f60d67911951cc87e58
Tue Munch Madsen <tuepvm@gmail.com>
2022-08-29 13:58:15 +0200

    Add project files.

---
+++ Elon's nye stasning/Car.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elon_s_nye_stasning
+{
+    internal class Car
+    {
+        //Instance Variables
+        private int battery;
+        private int meterPerBatteryProcent;
+        private string _color;
+        private int metersDriven;
+        private int timesToDrive;
+
+        //Get set
+        public int Battery
+        {
+            get { return battery; }
+            set { battery = value; }
+        }
+        public int MeterPerBatteryProcent
+        {
+            get { return  meterPerBatteryProcent; }
+            set {  meterPerBatteryProcent = value; }
+        }
+        public string Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+        public int MetersDriven
+        {
+            get { return metersDriven; }
+            set { metersDriven = value; }
+        }
+
+        public int TimesToDrive
+        {
+            get { return timesToDrive; }
+            set { timesToDrive = value; }
+        }
+
+
+        public void Drive()
+        {
+            if (battery > 0)
+            {
+                for (int i = 0; i < timesToDrive; i++)
+                {
+                    battery--;
+                    metersDriven += meterPerBatteryProcent;
+                }
+            }
+            else
+            {
+                Console.WriteLine("needs to be charged before driving again!");
+            }
+        }
+
+        //Charge method to charge the car battery to 100%
+        private void Charge()
+        {
+            Thread.Sleep(4000);
+            battery = 100;
+            metersDriven = 0;
+        }
+    }
+}
---
+++ Elon's nye stasning/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Elon_s_nye_stasning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creates 2 car objekts
+            Car redCar = new Car();
+            Car blueCar = new Car();
+
+            //Changes the color and how long the red car drives on 1% battery
+            redCar.Color = "Red";
+            redCar.MeterPerBatteryProcent = 20;
+
+            //Changes the color and how long the blue car drives on 1% battery
+            blueCar.Color = "Blue";
+            blueCar.MeterPerBatteryProcent = 10;
+            
+
+
+
+
+
+        }
+    }
+}
