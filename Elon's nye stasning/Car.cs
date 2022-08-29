2b4365828bc5c0034f8e88cfcd47de61e37a5b23
StationaryCaffeine <CaptPandaa@gmail.com>
2022-08-29 14:36:52 +0200

    Changes 1.1

--- Elon's nye stasning/Car.cs
+++ Elon's nye stasning/Car.cs
@@ -43,25 +43,30 @@ namespace Elon_s_nye_stasning
             set { timesToDrive = value; }
         }
 
-
+        //A method called drive that makes the car drive
         public void Drive()
         {
+            //Checks if the battery is above 0% power
             if (battery > 0)
             {
                 for (int i = 0; i < timesToDrive; i++)
                 {
+                    //Makes sure it doesn't count as driving when on 0% power
                     if (battery > 0)
                 	{
                         battery--;
-                       metersDriven += meterPerBatteryProcent;
+                        metersDriven += meterPerBatteryProcent;
 	                }
                 }
+                //Writes the display for the car
                 Console.WriteLine($"Den {Color} bil har {battery}% tilbage");
                 Console.WriteLine($"Den {Color} bil har kørt {metersDriven} meter");
                 Console.WriteLine("");
             }
             else
             {
+                //no more power
+                //You should use the charge method
                 Console.WriteLine("needs to be charged before driving again!");
             }
         }
@@ -69,6 +74,7 @@ namespace Elon_s_nye_stasning
         //Charge method to charge the car battery to 100%
         private void Charge()
         {
+            //a small wait like it would be if it wasn't a program
             Thread.Sleep(4000);
             battery = 100;
             Console.WriteLine("Battery is now at 100% power");
